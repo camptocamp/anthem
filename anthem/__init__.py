e55856793a50afd611fc1f2567ac541858c33f68
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-06-14 17:53:54 +0200

    fix link in copyright notices

--- anthem/__init__.py
+++ anthem/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Camptocamp SA
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html)
 
 __version__ = "0.1.0.dev0"
--- anthem/cli.py
+++ anthem/cli.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Camptocamp SA
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html)
 import argparse
 
 
