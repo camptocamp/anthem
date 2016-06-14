6d771b2e16835ca0b706b8f906f5dcfa9e9fede3
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2016-06-14 14:29:34 +0200

    structure the python package

---
+++ anthem/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+__version__ = "0.1.0.dev0"
---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import re
+
+from setuptools import setup, find_packages
+
+with open('anthem/__init__.py') as f:
+    version = re.search('^__version__\s*=\s*"(.*)"', f.read(), re.M).group(1)
+with open('README.rst') as f:
+    readme = f.read()
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+setup(
+    name='anthem',
+    version=version,
+    description='Make your Odoo scripts sing.',
+    long_description=readme + '\n\n' + history,
+    author='Camptocamp',
+    author_email='info@camptocamp.com',
+    url='https://github.com/camptocamp/anthem',
+    license='LGPLv3+',
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': ['anthem = anthem.cli:main']
+    },
+    classifiers=(
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: '
+        'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ),
+)
