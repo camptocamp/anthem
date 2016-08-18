c52a8315514ef855f723c7bfdb4585428931f590
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-08-18 21:10:46 +0200

    Add tests for the loaders and records lyrics

---
+++ tests/test_lyrics_loaders.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html)
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import pytest
+
+from anthem.exceptions import AnthemError
+import anthem.cli
+from anthem.lyrics.loaders import load_csv, load_csv_stream
+
+csv_partner = ("id,name,street,city\n"
+               "__test__.partner1,Partner 1,Street 1,City 1\n"
+               "__test__.partner2,Partner 2,Street 2,City 2\n"
+               )
+
+
+def test_load_csv_stream():
+    csv_stream = StringIO()
+    csv_stream.write(csv_partner)
+    csv_stream.seek(0)
+    with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        load_csv_stream(ctx, 'res.partner', csv_stream, delimiter=',')
+        partner1 = ctx.env.ref('__test__.partner1', raise_if_not_found=False)
+        assert partner1
+        assert partner1.name == 'Partner 1'
+        partner2 = ctx.env.ref('__test__.partner2', raise_if_not_found=False)
+        assert partner2
+        assert partner2.name == 'Partner 2'
+
+
+def test_load_csv_file(tmpdir):
+    csvfile = tmpdir.mkdir("files").join("res.partner.csv")
+    csvfile.write(csv_partner)
+    with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        load_csv(ctx, 'res.partner', csvfile.strpath, delimiter=',')
+        partner1 = ctx.env.ref('__test__.partner1', raise_if_not_found=False)
+        assert partner1
+        assert partner1.name == 'Partner 1'
+        partner2 = ctx.env.ref('__test__.partner2', raise_if_not_found=False)
+        assert partner2
+        assert partner2.name == 'Partner 2'
+
+
+def test_load_erroneous_csv():
+    err_csv = ("id,name,category_id/id\n"
+               "__test__.partner_fail,Test, xmlid_not_found\n")
+    csv_stream = StringIO()
+    csv_stream.write(err_csv)
+    csv_stream.seek(0)
+    with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        with pytest.raises(AnthemError):
+            load_csv_stream(ctx, 'res.partner', csv_stream, delimiter=',')
---
+++ tests/test_lyrics_records.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html)
+
+import anthem.cli
+from anthem.lyrics.records import add_xmlid, create_or_update
+
+
+def test_add_xmlid():
+    with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        env = ctx.env
+        record = env['res.partner'].create({'name': 'test'})
+        assert not record.get_metadata()[0]['xmlid']
+        add_xmlid(ctx, record, 'test.add_xmlid')
+        assert record.get_metadata()[0]['xmlid'] == 'test.add_xmlid'
+
+
+def test_create_or_update():
+    with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        record = create_or_update(ctx, 'res.partner', 'test.upsert',
+                                  {'name': 'test'})
+        assert record.name == 'test'
+        assert record._name == 'res.partner'
+        record2 = create_or_update(ctx, 'res.partner', 'test.upsert',
+                                   {'name': 'test2'})
+        assert record2 == record
+        assert record2.name == 'test2'
