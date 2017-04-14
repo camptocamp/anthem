d1ed0e2c4d1c35a08ad56c4d24b40d7216d22af3
Damien Crier <damien.crier@camptocamp.com>
2017-04-14 14:09:44 +0200

    check if module list is provided

--- anthem/lyrics/uninstaller.py
+++ anthem/lyrics/uninstaller.py
@@ -7,6 +7,10 @@ from ..exceptions import AnthemError
 
 def uninstall(ctx, module_list):
     """ uninstall module """
+    if not module_list:
+        raise AnthemError(u"You have to provide a list of "
+                          "module's name to uninstall")
+
     mods = ctx.env['ir.module.module'].search([('name', 'in', module_list)])
     try:
         mods.button_immediate_uninstall()
