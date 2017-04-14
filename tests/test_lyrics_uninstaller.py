d4ef645969a99ab2b71ee40506bcd03575efc060
Damien Crier <damien.crier@camptocamp.com>
2017-04-14 15:19:59 +0200

    install 'lunch' module during test and not at db creation

--- tests/test_lyrics_uninstaller.py
+++ tests/test_lyrics_uninstaller.py
@@ -9,6 +9,8 @@ from anthem.exceptions import AnthemError
 
 def test_uninstall_1():
     with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        ctx.env['ir.module.module'].search(
+            [('name', '=', 'lunch')]).button_immediate_install()
         try:
             uninstall(ctx, [])
         except AnthemError:
@@ -19,4 +21,6 @@ def test_uninstall_1():
 
 def test_uninstall_2():
     with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
+        ctx.env['ir.module.module'].search(
+            [('name', '=', 'lunch')]).button_immediate_install()
         uninstall(ctx, ['lunch'])
