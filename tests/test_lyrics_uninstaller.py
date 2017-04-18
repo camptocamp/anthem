701f061176dcba9a9466ffdbcbcb5b2a63189459
Damien Crier <damien.crier@camptocamp.com>
2017-04-18 09:19:19 +0200

    fix error handling

--- tests/test_lyrics_uninstaller.py
+++ tests/test_lyrics_uninstaller.py
@@ -2,6 +2,7 @@
 # Copyright 2016 Camptocamp SA
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html)
 
+import pytest
 import anthem.cli
 from anthem.lyrics.uninstaller import uninstall
 from anthem.exceptions import AnthemError
@@ -9,14 +10,9 @@ from anthem.exceptions import AnthemError
 
 def test_uninstall_1():
     with anthem.cli.Context(None, anthem.cli.Options(test_mode=True)) as ctx:
-        ctx.env['ir.module.module'].search(
-            [('name', '=', 'lunch')]).button_immediate_install()
-        try:
+        with pytest.raises(AnthemError) as excinfo:
             uninstall(ctx, [])
-        except AnthemError:
-            pass
-        else:
-            assert False
+        excinfo.match(r'You have to provide a list of module.*')
 
 
 def test_uninstall_2():
