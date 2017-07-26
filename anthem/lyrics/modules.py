45c23ebf60754313a36360fbc22c1a25576cc905
Denis Leemann <denis.leemann@camptocamp.com>
2017-07-26 13:24:19 +0200

    Move to models.py

---
+++ anthem/lyrics/modules.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from ..exceptions import AnthemError
+
+
+def uninstall(ctx, module_list):
+    """ uninstall module """
+    if not module_list:
+        raise AnthemError(u"You have to provide a list of "
+                          "module's name to uninstall")
+
+    mods = ctx.env['ir.module.module'].search([('name', 'in', module_list)])
+    try:
+        mods.button_immediate_uninstall()
+    except:
+        raise AnthemError(u'Cannot uninstall modules. See the logs')
+
+
+def update_translations(ctx, module_list):
+    """ Update translations from module list"""
+    if not module_list:
+        raise AnthemError(u"You have to provide a list of "
+                          "module's name to update the translations")
+
+    for module in module_list:
+        ctx.env['ir.module.module'].with_context(overwrite=True).search(
+            [('name', '=', module)]).update_translations()
