16d2c388d30026d30929c3230ada3bde69bf472d
Simone Orsi <simahawk@gmail.com>
2017-07-28 11:33:57 +0200

    [add] context manager to switch company

--- anthem/__init__.py
+++ anthem/__init__.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Camptocamp SA
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html)
 
-__version__ = "0.9.0.dev0"
+__version__ = "0.9.0"
 
 # publish the decorator so we can use 'anthem.log'
 from output import log  # noqa
