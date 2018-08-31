86567994d08b1b3d22e0a6a066ef7eec4d7a3011
Patrick Tombez <patrick.tombez@camptocamp.com>
2018-08-31 14:12:24 +0200

    Make update_translations Odoo 11 compatible

--- anthem/lyrics/modules.py
+++ anthem/lyrics/modules.py
@@ -23,8 +23,20 @@ def update_translations(ctx, module_list, overwrite=False):
     if not isinstance(module_list, list):
         raise AnthemError(u"You have to provide a list of "
                           "module's name to update the translations")
+    if overwrite:
+        ctx.log_line(
+            u'All previous translations will be dropped for requested addons'
+        )
+    ir_module = ctx.env['ir.module.module']
+    if hasattr(ir_module, 'update_translations'):
+        # Odoo version <= 10.0
+        method_name = 'update_translations'
+    else:
+        # Odoo version >= 11.0
+        method_name = '_update_translations'
 
     domain = [('name', 'in', module_list)]
     mods = ctx.env['ir.module.module'].search(domain)
     ctx.log_line('Reloading translations for %s' % str(module_list))
-    mods.with_context(overwrite=overwrite).update_translations()
+    mods.with_context(overwrite=overwrite)
+    getattr(mods, method_name)()
