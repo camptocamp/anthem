67e8565b7d597f614777aba5e1686c039d083f95
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-11-22 15:18:45 +0100

    Add tracking_disable=True as default context to load CSVs
    closes #42

--- anthem/lyrics/loaders.py
+++ anthem/lyrics/loaders.py
@@ -57,7 +57,7 @@ def read_csv(data, dialect='excel', encoding='utf-8', **fmtparams):
 
 def load_rows(ctx, model, header, rows):
     if isinstance(model, basestring):
-        model = ctx.env[model]
+        model = ctx.env[model].with_context(tracking_disable=True)
     result = model.load(header, rows)
     ids = result['ids']
     if not ids:
